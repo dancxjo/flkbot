6ac322948674e4807d3c02fc78458b700cd8d46b
Dancxjo <tdreed@gmail.com>
2017-10-02 09:32:31 -0700

    Add debug raw events

--- src/index.js
+++ src/index.js
@@ -8,8 +8,15 @@ const channelNam = process.env.SPRAKAR_NAM || '#folkbot';
 const client = new irc.Client(server, minNam, {channels: [channelNam]});
 
 client.addListener('error', function(message) {
-    console.error('error: ', message);
+    console.log('error: ', message);
 });
+
+if (process.env.DEBUG) {
+    client.addListener('raw', function(message) {
+        console.log('raw: ', message);
+    });
+}
+
 
 client.addListener('registered', function(message) {
     console.log('registered: ', message);
