1276c3d467bb6a880236e0c845e707e3fc226842
Dancxjo <tdreed@gmail.com>
2017-10-02 09:57:55 -0700

    Add SASL

--- src/index.js
+++ src/index.js
@@ -4,8 +4,16 @@ const Chatbot = require('./chatbot');
 const server = process.env.SERVER || 'irc.freenode.net';
 const minNam = process.env.SPREKAR_NAM || 'folkbot';
 const channelNam = process.env.CHAN_NAM || '#folkbot';
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
 
-const client = new irc.Client(server, minNam, {channels: [channelNam]});
+
+const client = new irc.Client(server, minNam, {
+    sasl: username ? true : undefined,
+    userName: username || undefined,
+    password: password || undefined,
+    channels: [channelNam]
+});
 
 client.addListener('error', function(message) {
     console.log('error: ', message);
